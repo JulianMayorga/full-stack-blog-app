6ee3513aa30911d9f774da3bba9edc762895f775
JulianMayorga <mayorga.julian@gmail.com>
2016-10-22 10:47:20 -0300

    Tomatos everywhere! (add color to titles)

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -10,8 +10,8 @@ export default class PostItem extends React.Component {
   render() {
     return (
       <section className={classNames(classes.PostItem, 'panel', 'panel-default')}>
-        <section className="panel-body">
-          <h1><Link className={classes.Title} to={`/posts/${this.props.post._id}`}>{this.props.post.editorState.blocks[0].text}</Link></h1>
+        <section className={classNames(classes.Header, 'panel-body')}>
+          <h1 className={classes.Title}><Link className={classes.Link} to={`/posts/${this.props.post._id}`}>{this.props.post.editorState.blocks[0].text}</Link></h1>
           <span className={classes.Date}>{new Date(this.props.post.date).toDateString()}</span>
         </section>
         <Link className={classes.ReadMore} to={`/posts/${this.props.post._id}`}>Read more...</Link>
