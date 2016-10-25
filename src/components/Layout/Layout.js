f31ae30dd3b82c96fee3c0301ce38558be24f208
JulianMayorga <mayorga.julian@gmail.com>
2016-10-24 21:54:32 -0300

    Removed header animation
    It was causing an annoying issue that mad it impossible to scroll past the title in large screens (at least with screens larger than mine).

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,47 +1,14 @@
 import React from 'react';
-import classNames from 'classnames';
-import throttle from 'lodash/throttle';
 
 import Header from '../Header';
 import Footer from '../Footer';
 
 import classes from './Layout.scss';
 
-function onScroll(onDown, onUp) {
-  let lastScrollTop = 0;
-  window.onscroll = throttle((ev) => {
-    if (window.scrollY > lastScrollTop) {
-      onDown(ev);
-    } else if (window.scrollY < lastScrollTop) {
-      onUp(ev);
-    }
-    lastScrollTop = window.scrollY;
-  });
-}
-
 export default class Layout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      direction: '',
-    };
-  }
-  componentDidMount() {
-    const onScrollDown = () => {
-      if (this.state.direction !== 'down') {
-        this.setState({ direction: 'down' });
-      }
-    };
-    const onScrollUp = (ev) => {
-      if (window.scrollY === 0) {
-        this.setState({ direction: 'up' });
-      }
-    };
-    onScroll(onScrollDown, onScrollUp);
-  }
   render() {
     return (
-      <div className={classNames(classes.Layout, this.state.direction)}>
+      <div className={classes.Layout}>
         <Header />
         {this.props.children}
         <Footer />
